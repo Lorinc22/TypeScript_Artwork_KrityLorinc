80117cd0eaa66eba1928d0cf35c18ff76bcb8663
Krity Lorinc <krity.lorinc@gmail.com>
2022-11-10 14:48:01 +0100

    index eleje

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let lista = [];
+document.addEventListener('DOMContentLoaded', () => {
+    let statue = document.getElementById('statue');
+    let artwork = document.getElementById('artwork');
+    let nev = document.getElementById('nev');
+    let ertek = document.getElementById('ertek');
+    let ev = document.getElementById('ev');
+    let magassag = document.getElementById('magassag');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 import { Statue } from "./Statue";
 import { Artwork } from "./Artwork";
+
+let lista:Artwork [] = [];
+
+document.addEventListener('DOMContentLoaded', () =>{
+
+    let statue = document.getElementById('statue') as HTMLInputElement;
+    let artwork = document.getElementById('artwork') as HTMLInputElement;
+    let nev = document.getElementById('nev') as HTMLInputElement;
+    let ertek = document.getElementById('ertek') as HTMLInputElement;
+    let ev = document.getElementById('ev') as HTMLInputElement;
+    let magassag = document.getElementById('magassag') as HTMLInputElement;
+
+
+
+
+})
